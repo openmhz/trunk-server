26033ce2d006093bda8ba350779981e7768c0470
Matthew Bowden <bowdenm@spu.edu>
2020-07-07 21:32:58 -0700

    Only scroll to top if user has not scrolled down

--- frontend/src/client/Call/ListCalls.js
+++ frontend/src/client/Call/ListCalls.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {Link} from 'react-router-dom';
 import CallItem from "./CallItem";
 import {
@@ -25,28 +25,28 @@ class ListCalls extends React.Component {
   constructor(props) {
     super(props);
 
+    // Ref for auto-scrolling
+    this.headerRef = React.createRef();
+    this.windowPos = 0;
   }
-  
-  
-  //https://stackoverflow.com/questions/33188994/scroll-to-the-top-of-the-page-after-render-in-react-js
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
- 
- 
+
+  componentDidMount() {
+    if (this.windowPos === 0) {
+      // If the user hasn't scrolled down intentionally, scroll up to the top of the page
+      this.headerRef.current.scrollTo(0, 0);
+    }
+  }
 
   //https://stackoverflow.com/questions/36559661/how-can-i-dispatch-from-child-components-in-react-redux
   //https://stackoverflow.com/questions/42597602/react-onclick-pass-event-with-parameter
   render() {
-
-
-  
-   
+    // Capture window position
+    this.windowPos = window.scrollY;
    
     return (
 
         <Table id="calls" unstackable>
-          <Table.Header >
+          <Table.Header ref={this.headerRef}>
             <Table.Row>
               <Table.HeaderCell>Len</Table.HeaderCell>
               <Table.HeaderCell>Talkgroup</Table.HeaderCell>
