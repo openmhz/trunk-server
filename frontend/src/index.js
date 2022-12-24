1cfbecdd1a83044b2cb31d6ffbbe8f441b9fca3b
Luke Berndt <lukekb@gmail.com>
2022-12-24 15:08:07 -0500

    update worked?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux'
-import { Route, Switch } from 'react-router' // react-router v4/v5
+import { Routes ,Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router'
 import configureStore, { history } from './redux-router/configureStore'
 import ReactGA from 'react-ga';
 
 
 import 'semantic-ui-css/semantic.min.css'
-
+/*
 // withTracker
 import withTracker from './withTracker';
 
@@ -21,25 +22,26 @@ import ListSystemsContainer from "./System/ListSystemsContainer"
 // Call
 import CallPlayerContainer from "./Call/CallPlayerContainer"
 
-import AboutComponent from "./About/AboutComponent"
+import AboutComponent from "./About/AboutComponent"*/
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
 
 const store = configureStore(/* provide initial state if any */)
-
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <ConnectedRouter history={history}> { /* place ConnectedRouter under Provider */ }
       <> { /* your usual react-router v4/v5 routing */ }
-        <Switch>
+        <Routes>
+          {/* 
           <Route exact path="/" component={withTracker(MainContainer)} />
           <Route exact path="/systems" component={withTracker(ListSystemsContainer)} />
           <Route exact path="/system/:shortName" component={withTracker(CallPlayerContainer)} />
-          <Route exact path="/about" component={withTracker(AboutComponent)} />
+          <Route exact path="/about" component={withTracker(AboutComponent)} />*/}
           <Route render={() => (<div>Miss</div>)} />
-        </Switch>
+        </Routes>
       </>
     </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
