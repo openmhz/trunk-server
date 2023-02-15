49ddc64a300069376c8864254369e1f60a2e8059
Luke Berndt <lukekb@gmail.com>
2023-02-14 22:13:03 -0500

    switch tracking

--- frontend/src/tracking.js
+++ frontend/src/tracking.js
@@ -4,12 +4,12 @@ import { useLocation } from "react-router-dom";
 
 export const usePageTracking = () => {
   const location = useLocation();
-
+/*
   useEffect(() => {
     window.gtag("event", "page_view", {
       page_path: location.pathname + location.search + location.hash,
       page_search: location.search,
       page_hash: location.hash,
     });
-  }, [location]);
+  }, [location]);*/
 };
