416b8dd7780cf8fdc0055e360569e54a7e67ab5c
Luke Berndt <luke@robotastic.com>
2024-02-03 09:50:55 -0500

    no need for https yet

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,11 +4,14 @@
 
 
 # Load environment variables from prod.env.example
-#if [ -f prod.env ]; then
-#    source prod.env
-#fi
+if [ -f prod.env ]; then
+    source prod.env
+else
+  echo 'Error: prod.env not found, copy prod.example.env to prod.env and fill it in.' >&2
+  exit 1
+fi
 
-source test.env
+
 
 # Rest of the script...
 
@@ -40,27 +43,27 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker compose -f docker-compose.yml -f test-compose.yml run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
+# echo "### Creating dummy certificate for $domains ..."
+# path="/etc/letsencrypt/live/$domains"
+# mkdir -p "$data_path/conf/live/$domains"
+# docker compose -f docker-compose.yml -f test-compose.yml run --rm --entrypoint "\
+#   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+#     -keyout '$path/privkey.pem' \
+#     -out '$path/fullchain.pem' \
+#     -subj '/CN=localhost'" certbot
+# echo
 
 
-echo "### Starting nginx ..."
-docker compose -f docker-compose.yml -f test-compose.yml up -d nginx
-echo
+ echo "### Starting nginx ..."
+ docker compose -f docker-compose.yml -f test-compose.yml up -d nginx
+ echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker compose -f docker-compose.yml -f test-compose.yml run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+# echo "### Deleting dummy certificate for $domains ..."
+# docker compose -f docker-compose.yml -f test-compose.yml run --rm --entrypoint "\
+#   rm -Rf /etc/letsencrypt/live/$domains && \
+#   rm -Rf /etc/letsencrypt/archive/$domains && \
+#   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+# echo
 
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
@@ -89,5 +92,5 @@ docker compose -f docker-compose.yml -f test-compose.yml run --rm --entrypoint "
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
-docker compose -f docker-compose.yml -f test-compose.yml exec nginx nginx -s reload
+#echo "### Reloading nginx ..."
+#docker compose -f docker-compose.yml -f test-compose.yml exec nginx nginx -s reload
