6d0335c2781d4a968e63210f587999c33b24e258
Luke Berndt <luke@robotastic.com>
2024-08-24 10:29:11 -0400

    fixed otel agent

--- backend/agents/otel-tracing.js
+++ backend/agents/otel-tracing.js
@@ -4,6 +4,19 @@ const opentelemetry = require('@opentelemetry/sdk-node');
 const { TraceIdRatioBasedSampler } = require('@opentelemetry/sdk-trace-node');
 const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-http');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+// const {
+//     dockerCGroupV1Detector,
+//   } = require('@opentelemetry/resource-detector-docker');
+
+// For troubleshooting, set the log level to DiagLogLevel.DEBUG
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.WARN);
+
+console.log("Starting OpenTelemetry Node SDK");
+console.log("OTEL_EXPORTER_OTLP_ENDPOINT: " + process.env.OTEL_EXPORTER_OTLP_ENDPOINT);
+console.log("OTEL_EXPORTER_OTLP_HEADERS: " + process.env.OTEL_EXPORTER_OTLP_HEADERS);
+console.log("OTEL_SERVICE_NAME: " + process.env.OTEL_SERVICE_NAME);
+
 
 const samplePercentage = 0.05;
 
@@ -19,6 +32,7 @@ const sdk = new opentelemetry.NodeSDK({
             },
         }),
     ],
+  //resourceDetectors: [dockerCGroupV1Detector],
 });
 
 sdk.start();
